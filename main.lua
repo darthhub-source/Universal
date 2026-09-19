85b49829737a5bedb91450dd2f94682ea41ffa84
darthhub-source <aevixhub@gmail.com>
2026-09-19 12:11:08 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/ZdjmgMFMuVtjkfpu/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Wisteriammy/-/refs/heads/main/mm2.lua"))()
